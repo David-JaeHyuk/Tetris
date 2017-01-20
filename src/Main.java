779130b97317ec30e8d0e31004374a23f9ee34ec
David-JaeHyuk <aspact@naver.com>
2017-01-20 18:16:59 +0900

    블럭, 블럭컨트롤, 보드 클래스 텅 빈 채 추가

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 
-public class Main {
-
-	public static void main(String[] args) {
+public class Main 
+{
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
+		MyView view = new MyView();
 		
+		view.setSize(300, 300);
+		view.setVisible(true);
 	}
 
 }
